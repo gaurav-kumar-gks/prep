c91bcc15157c95ec7c930d6f28c112b610ecd48e
Gaurav Kumar <42230870+gaurav-kumar-gks@users.noreply.github.com>
2024-11-18 12:07:29 +0530

    fix singleton thread safe metaclass

--- design-patterns/creational/singleton.py
+++ design-patterns/creational/singleton.py
@@ -76,20 +76,12 @@ class SingletonMetaThreadSafe(type):
         Possible changes to the value of the `__init__` argument do not affect
         the returned instance.
         """
-        # Now, imagine that the program has just been launched. Since there's no
-        # Singleton instance yet, multiple threads can simultaneously pass the
-        # previous conditional and reach this point almost at the same time. The
-        # first of them will acquire lock and will proceed further, while the
-        # rest will wait here.
-        with cls._lock:
-            # The first thread to acquire the lock, reaches this conditional,
-            # goes inside and creates the Singleton instance. Once it leaves the
-            # lock block, a thread that might have been waiting for the lock
-            # release may then enter this section. But since the Singleton field
-            # is already initialized, the thread won't create a new object.
-            if cls not in cls._instances:
-                instance = super().__call__(*args, **kwargs)
-                cls._instances[cls] = instance
+
+        if cls not in cls._instances:
+            with cls._lock:
+                if cls not in cls._instances:
+                    instance = super().__call__(*args, **kwargs)
+                    cls._instances[cls] = instance
         return cls._instances[cls]
 
 
